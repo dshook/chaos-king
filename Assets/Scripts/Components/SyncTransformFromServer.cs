84f35c148e50b6b98315c78a45494d598e3dd8c9
Dillon Shook <dshook@alumni.nmt.edu>
2015-08-05 18:21:18 -0400

    Enemy Movement synced

---
+++ Assets/Scripts/Components/SyncTransformFromServer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncTransformFromServer : NetworkBehaviour
+{
+
+    [SyncVar]
+    private Vector3 syncPos;
+    [SyncVar]
+    private float syncYRot;
+
+    private Vector3 lastPos;
+    private Quaternion lastRot;
+    private Transform myTransform;
+    private float lerpRate = 10;
+    private float posThreshold = 0.5f;
+    private float rotThreshold = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TransmitMotion();
+        LerpMotion();
+    }
+
+    void TransmitMotion()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(myTransform.position, lastPos) > posThreshold || Quaternion.Angle(myTransform.rotation, lastRot) > rotThreshold)
+        {
+            lastPos = myTransform.position;
+            lastRot = myTransform.rotation;
+
+            syncPos = myTransform.position;
+            syncYRot = myTransform.localEulerAngles.y;
+        }
+    }
+
+    void LerpMotion()
+    {
+        if (isServer)
+        {
+            return;
+        }
+
+        myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+
+        Vector3 newRot = new Vector3(0, syncYRot, 0);
+        myTransform.rotation = Quaternion.Lerp(myTransform.rotation, Quaternion.Euler(newRot), Time.deltaTime * lerpRate);
+    }
+}
